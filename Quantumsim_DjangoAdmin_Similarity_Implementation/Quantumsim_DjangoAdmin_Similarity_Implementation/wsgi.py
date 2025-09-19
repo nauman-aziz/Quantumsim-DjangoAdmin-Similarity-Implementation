af8bcb8c4c5a09a1086f97ecff2721ba7e0945d6
Nauman Aziz <mnauman@quantumgears.com>
2025-09-19 13:30:24 +0500

    Initial Django project with Unfold setup

---
+++ Quantumsim_DjangoAdmin_Similarity_Implementation/Quantumsim_DjangoAdmin_Similarity_Implementation/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for Quantumsim_DjangoAdmin_Similarity_Implementation project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "Quantumsim_DjangoAdmin_Similarity_Implementation.settings",
+)
+
+application = get_wsgi_application()
